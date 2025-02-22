7f9409b0c23a983a0347736f1202d5333ea9bd90
Sayantan <Sayantanb595@gmail.com>
2025-02-22 18:59:35 +0530

    Added FootNotes

--- hello.js
+++ hello.js
@@ -1,3 +1,11 @@
 // This is the master branch
 // hello.js -> test.js -> readme.md
 console.log("Helllo , Git!");
+// Any changes in a file needs to be staged within the master branch
+// 1. ADD the change: git add ./ -> adds the changes
+// 2. Check the status: git status -> check if the changes are staged 
+/* 3. Commit the changes:
+        git commit -m "Your message" -> commits the changes
+        git push origin master -> pushes the changes to the remote repository
+*/
+
